03f0638816a9899f91d20b3a8d04b5f6d2cd4d32
silinaleksei <silinaleksei@gmail.com>
2022-08-29 07:22:57 +0300

    Homework Lesson Change conditions in the method printColor (add 'else')

--- src/lesson1/HomeWorkApp.java
+++ src/lesson1/HomeWorkApp.java
@@ -26,14 +26,12 @@ public class HomeWorkApp {
   }
 
   public static void printColor() {
-    int value = 101;
+    int value = 0;
     if (value <= 0) {
       System.out.println("Red");
-    }
-    if (value > 0 && value <= 100) {
+    } else if (value > 0 && value <= 100) {
       System.out.println("Yellow");
-    }
-    if (value > 100) {
+    } else if (value > 100) {
       System.out.println("Green");
     }
   }
